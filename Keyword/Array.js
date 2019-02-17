9f87cee673b528c221c798b7a96bdbcbe86bab2d
hongs <zhdqls@gmail.com>
2019-02-17 12:49:37 +0900

    saving for middle's work

---
+++ Keyword/Array.js
@@ -0,0 +1,24 @@
+//just for trying to make my own array in real.
+var 해야하는일 = [ 
+    "직장",
+    "코딩공부",
+    "운동",
+    "데이트",
+    "책읽기",
+    "책쓰기",
+    ]
+console.log(해야하는일[0]);
+
+var 화가난다 = [ ];
+화가난다[0] = "짜증1"
+화가난다[1] = "짜증2"
+console.log(화가난다);
+
+var 친구 = [ ];
+친구.push("신혜");
+친구.push("정은");
+console.log(친구);
+
+//배열 원소 색인 찾기
+var 숫자 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(숫자.indexOf(9));
--- Keyword/Boolean.js
+++ Keyword/Boolean.js
@@ -1,3 +1,15 @@
 var 주말인가요 = true;
 var 샤워해야하나요 = !주말인가요;
 console.log(샤워해야하나요);
+
+
+var 나이 = 11;
+var 어른동반 = true;
+var 영화관람 = 11;
+console.log(( 나이 > 영화관람 ) || 어른동반 );
+
+나이 = 12;
+어른동반 = false;
+console.log((나이 > 영화관람) || 어른동반 );
+
+
