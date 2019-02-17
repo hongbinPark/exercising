34362d29d7d2b26eb2915e4cb67e760af60a3f95
hongs <zhdqls@gmail.com>
2019-02-17 14:51:18 +0900

    The socond practcing

---
+++ Keyword/IForIFELSE.js
@@ -0,0 +1,27 @@
+var 이름 = "레오나르도 디카프리오";
+console.log("안녕하세요. " + 이름 + "님" );
+if(이름.length > 7) {
+    console.log("우와, 정말 긴 이름이네요!");
+} else {
+     console.log("엄청 긴 이름은 아니네요.");
+}
+
+
+
+
+if (조대포) {
+    console.log("오늘은 고기 먹는 날!");
+} else if (용초수) {
+    console.log("꿔버러우와 소고기면은 진리지!");
+} else if (이수영피자) {
+    console.log("오늘은 왠지 피자가 땡기는구나?");
+} else if (코케코코) {
+    console.log("건강한 연어로 맛있게 단백질 보충하자!");
+} else {
+    console.log("그냥 굶어.");
+}
+
+var 조대포 = false;
+var 용초수 = false;
+var 이수영피자 = false;
+var 코케코코 = false;
